e0d4d8eabc7ef7ecbb21011091a312d474c80922
Miendjem Thierry <miendjemthierry01@gmail.com>
2022-05-07 23:00:38 +0100

    fsadf

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -4,17 +4,36 @@ import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import { ContactStackNavigator } from "../navigation"
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TabNavigator from "./TabNavigator";
+import SplashScreen from "../screens/splash/WelcomeScreen"
 
 const Drawer = createDrawerNavigator();
+
+const Stack = createNativeStackNavigator()
+
+const screenOptions = {
+    headerShown: false,
+}
+
 
 
 const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={TabNavigator}  />
-      <Drawer.Screen name="Contact" component={ContactStackNavigator} />
-    </Drawer.Navigator>
+      <>
+      
+      <Drawer.Navigator>
+      <Stack.Screen name="SplashScreen" component={SplashScreen}/>
+                <Drawer.Screen name="Home" component={TabNavigator} />
+                <Drawer.Screen name="Contact" component={ContactStackNavigator} />
+            </Drawer.Navigator>
+
+        {/* <Stack.Navigator initialRouteName="SchoolIndexScreen" screenOptions={screenOptions}>
+            <Stack.Screen name="SplashScreen" component={SplashScreen}/>
+            
+
+        </Stack.Navigator> */}
+      </>
   );
 }
 
---
+++ navigation/stack/Authentication.js
@@ -0,0 +1,67 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable comma-dangle */
+/* eslint-disable eol-last */
+/* eslint-disable semi */
+import React, { useContext } from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import 'react-native-gesture-handler';
+
+//splash screens
+import SplashScreen from './screens/splash/WelcomeScreen'
+
+//Authentication screen
+import LoginScreen from './screens/authentication/LoginScreen'
+import ChangePasswordScreen from './screens/authentication/ChangePasswordScreen'
+import OTPScreen from './screens/authentication/OTPScreen';
+import SendEmailScreen from './screens/authentication/SendEmailScreen';
+
+// courses screen
+import ListCourseScreen from './screens/school/class/ListCourseScreen';
+import SchoolIndexScreen from './screens/school/SchoolIndexScreen';
+import ListAccountScreen from './screens/school/ListAccountScreen';
+import { AuthContext, AuthProvider } from './module/auth/action';
+import About from './screens/school/About';
+import Contact from './screens/school/Contact';
+
+
+const Stack = createNativeStackNavigator()
+
+const screenOptions = {
+    headerShown: false,
+}
+
+
+const AuthenticationStack = () => {
+
+    return (
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="SchoolIndexScreen" screenOptions={screenOptions}>
+            {/* Splash screens */}
+            <Stack.Screen name="SplashScreen" component={SplashScreen}/>
+
+            {/* Authentication screen */}
+            
+            <Stack.Group screenOptions={{ presentation: 'modal' }}>
+                <Stack.Screen name="LoginScreen" component={LoginScreen} />
+                <Stack.Screen name="ChangePasswordScreen" component={ChangePasswordScreen} />
+                <Stack.Screen name="OTPScreen" component={OTPScreen} />
+                <Stack.Screen name="SendEmailScreen" component={SendEmailScreen} />
+                <Stack.Screen name="ListAccountScreen" component={ListAccountScreen} />
+            </Stack.Group>
+
+            {/* courses screens */}
+            {/* <Stack.Screen name="SchoolIndexScreen" component={SchoolIndexScreen} /> */}
+            <Stack.Screen name="ListCourseScreen" component={ListCourseScreen} />
+
+
+            
+            {/* <Stack.Screen name="About" component={About} /> */}
+            
+        </Stack.Navigator>
+    </NavigationContainer>
+)}
+
+export default AuthenticationStack
