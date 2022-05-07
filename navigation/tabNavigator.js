bc36b308d26a50069a3b2886554226bcbdc3e306
Miendjem Thierry <miendjemthierry01@gmail.com>
2022-05-07 16:48:54 +0100

    change

---
+++ navigation/tabNavigator.js
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable comma-dangle */
+/* eslint-disable eol-last */
+/* eslint-disable semi */
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { MainStackNavigator, ContactStackNavigator } from "../navigation";
+
+const Tab = createBottomTabNavigator();
+
+const screenOptions = {
+  headerShown: true,
+}
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator screenOptions={screenOptions}>
+      <Tab.Screen name="Home" component={MainStackNavigator} />
+      <Tab.Screen name="Contact" component={ContactStackNavigator} />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTabNavigator;
